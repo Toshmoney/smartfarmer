d7ee5cc85adf5194373fe03ca1599cd191e1397c
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-08-12 19:45:15 -0400

    updating signup page

--- client-side/src/Components/Signup/Content.jsx
+++ client-side/src/Components/Signup/Content.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import password from '../../Images/Vector (10).png'
+
+
 
 const Content = () => {
   return (
@@ -14,10 +17,11 @@ const Content = () => {
             <div className='flex flex-col gap-4'>
                 <input className=' border-grey w-full py-3 outline-none border-b-2 nunito' type="text" placeholder='Full Name' />
                 <input className=' border-grey w-full py-3 outline-none border-b-2 nunito' type="text" placeholder='Email Address' />
-                <div className='border-grey w-full border-b-2'>
+                <div className='border-grey w-full border-b-2 flex items-center'>
                     <input className=' border-grey w-full py-3 outline-none nunito' type="text" placeholder='Password' />
+                    <img src={password} alt="" />
                 </div>
-                <b className='font-small'>By Signing Mean You agree To Our <span><a href=""><u>Terms of Services</u></a></span></b>
+                <b className='font-small'>By Signing Mean You agree To Our <span className='text-[##35CD8D]'><a href=""><u>Terms of Services</u></a></span></b>
                 <button className='btn text-white'>Login</button>
                
             </div>
