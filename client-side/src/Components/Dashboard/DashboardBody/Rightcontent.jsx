007300fe6280fdaa3e81931768931429b1ed9d77
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-08-14 07:22:14 -0400

    updating Dashboard

--- client-side/src/Components/Dashboard/DashboardBody/Rightcontent.jsx
+++ client-side/src/Components/Dashboard/DashboardBody/Rightcontent.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import chart from '../../../Images/Chart 7.png'
+import del from '../../../Images/Vector (11).png'
+
 
 const Rightcontent = () => {
   return (
@@ -25,7 +27,7 @@ const Rightcontent = () => {
 
       <div className='py-10 px-10 bg-white rounded-3xl'>
         <h2 className='capitalize nunito'>payment history</h2>
-        <table className='w-full flex flex-col'>
+        <table className='w-full flex flex-col gap-3'>
           <div className='flex flex-row'>
             <h3 className='name'>Recipent Name</h3>
             <h3 className='others'>Bank Name</h3>
@@ -33,15 +35,55 @@ const Rightcontent = () => {
             <h3 className='others'>Date</h3>
             <h3 className='others'>Action</h3>
           </div>
-          <tr className='flex flex-row'>
+          <tr className='flex flex-row items-center'>
             <td className='name flex gap-4'>
-              <li className='list-none'>1</li>
-              <h5>Ibrahim Abdulsalam</h5>
+              <li className='list-none font-bold border-2 py-1 px-3 rounded-lg'>1</li>
+              <h4>Ibrahim Abdulsalam</h4>
             </td>
-            <td className='others'>WEMA Bank</td>
-            <td className='others'>NGN 200, 000</td>
-            <td className='others'>26, March 2023</td>
-            <td className='others'>Icon</td>
+            <td className='others font-bold'>WEMA Bank</td>
+            <td className='others font-bold'>NGN 200, 000</td>
+            <td className='others font-bold'>26, March 2023</td>
+            <td className='others font-bold'><img src={del} alt="" /></td>
+          </tr>
+          <tr className='flex flex-row items-center'>
+            <td className='name flex gap-4'>
+              <li className='list-none font-bold border-2 py-1 px-3 rounded-lg'>2</li>
+              <h4>Ibrahim Abdulsalam</h4>
+            </td>
+            <td className='others font-bold'>OPAY Bank</td>
+            <td className='others font-bold'>NGN 200, 000</td>
+            <td className='others font-bold'>26, March 2023</td>
+            <td className='others font-bold'><img src={del} alt="" /></td>
+          </tr>
+          <tr className='flex flex-row items-center'>
+            <td className='name flex gap-4'>
+              <li className='list-none font-bold border-2 py-1 px-3 rounded-lg'>2</li>
+              <h4>Ibrahim Abdulsalam</h4>
+            </td>
+            <td className='others font-bold'>WEMA Bank</td>
+            <td className='others font-bold'>NGN 200, 000</td>
+            <td className='others font-bold'>26, March 2023</td>
+            <td className='others font-bold'><img src={del} alt="" /></td>
+          </tr>
+          <tr className='flex flex-row items-center'>
+            <td className='name flex gap-4'>
+              <li className='list-none font-bold border-2 py-1 px-3 rounded-lg'>2</li>
+              <h4>Ibrahim Abdulsalam</h4>
+            </td>
+            <td className='others font-bold'>OPAY Bank</td>
+            <td className='others font-bold'>NGN 200, 000</td>
+            <td className='others font-bold'>26, March 2023</td>
+            <td className='others font-bold'><img src={del} alt="" /></td>
+          </tr>
+          <tr className='flex flex-row items-center'>
+            <td className='name flex gap-4'>
+              <li className='list-none font-bold border-2 py-1 px-3 rounded-lg'>2</li>
+              <h4>Ibrahim Abdulsalam</h4>
+            </td>
+            <td className='others font-bold'>PALMPAY Bank</td>
+            <td className='others font-bold'>NGN 200, 000</td>
+            <td className='others font-bold'>26, March 2023</td>
+            <td className='others font-bold'><img src={del} alt="" /></td>
           </tr>
         </table>
       </div>
