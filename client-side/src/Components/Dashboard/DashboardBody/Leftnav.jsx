066c8e2d25e60a2dcc7b8d059a9e1df9032f787f
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-04 17:32:24 -0400

    adjusted mobile nav

--- client-side/src/Components/Dashboard/DashboardBody/Leftnav.jsx
+++ client-side/src/Components/Dashboard/DashboardBody/Leftnav.jsx
@@ -1,108 +1,128 @@
-import React, { useState } from 'react'
-import {LuLayoutDashboard} from 'react-icons/lu'
+import React, { useState, useEffect } from 'react'
+import { LuLayoutDashboard } from 'react-icons/lu'
 import toggle from '../../../Images/Group 20.png'
-import {GiThreeLeaves} from 'react-icons/gi'
-import {BiHistory, BiLogOutCircle} from 'react-icons/bi'
-import {FaAmazonPay} from 'react-icons/fa'
-import {AiOutlineSetting} from 'react-icons/ai'
-import {PiListNumbersBold} from 'react-icons/pi'
-import {GiFarmTractor} from 'react-icons/gi'
+import { GiThreeLeaves } from 'react-icons/gi'
+import { BiHistory, BiLogOutCircle } from 'react-icons/bi'
+import { FaAmazonPay } from 'react-icons/fa'
+import { AiOutlineSetting } from 'react-icons/ai'
+import { PiListNumbersBold } from 'react-icons/pi'
+import { GiFarmTractor } from 'react-icons/gi'
 
 const Leftnav = () => {
     const [tog, setTog] = useState(false)
-    const [open, setOpen] = useState(false)
 
-    const handleDashboard = () =>{
+    const handleDashboard = () => {
         setTog(!tog)
     }
-    const handleTracktor = () =>{
-        setOpen(!open)
-    }
+    // const handleTracktor = () => {
+    //     setOpen(!open)
+    // }
+    // useEffect(()=>{
+    //     const handleShowTracktor = () => {
+    //         if (document.body.scrollTop >= 4 || document.documentElement.scrollTop >= 4) {
+    //             setHideTracktor(false)
+    //         } else {
+    //             setHideTracktor(true);
+    //         }
+    //     }
 
-  return (
-    <>
-        <div className={tog? "navigation-after":'hidden lg:bg-white lg:w-[15%] lg:h-[92.5vh] lg:flex lg:flex-col gap-[2.5rem]'}>
-            <div>
-                <img onClick={handleDashboard} className='relative w-[50px] h-[50px] left-[87%] cursor-pointer top-4' src={toggle} alt="" />
+    //     // window.addEventListener("scroll", handleShowTracktor());
+    //     window.addEventListener("scroll", handleShowTracktor());
+
+    //     return () => {
+    //         window.removeEventListener("scroll", handleShowTracktor)
+    //     }
+    // }, [])
+
+    return (
+        <>
+            <div className={tog ? "navigation-after" : 'hidden lg:bg-white lg:w-[15%] lg:h-[92.5vh] pt-[5%] lg:flex lg:flex-col gap-[2.5rem] relative'}>
+                <button className='absolute w-[50px] h-[50px] left-[91%] cursor-pointer top-[4%]'>
+                    <img onClick={handleDashboard}  src={toggle} alt="" />
+                </button>
+                <div className='flex flex-col h-[100%] justify-between'>
+                    <div className='pl-3 lg:flex flex-col gap-12 px-3'>
+                        <a href='dashboard' className='flex gap-2 items-center '>
+                            <LuLayoutDashboard className='icon' />
+                            <p className={tog ? "title" : 'nunito text-[#35CD8D] text-[20px]'}>Dashboard</p>
+                        </a>
+                        <a href='farm-details' className='flex gap-2 items-center'>
+                            <GiThreeLeaves className='icon' />
+                            <p className={tog ? "title" : 'nunito text-[grey] text-[20px]'}>Farm Details</p>
+                        </a>
+                        <a href='loan-application' className='flex gap-2 items-center'>
+                            <PiListNumbersBold className='icon' />
+                            <p className={tog ? "title" : 'nunito text-[grey] text-[20px]'}>Loan Application</p>
+                        </a>
+                        <a href='repayment' className='flex gap-2 items-center'>
+                            <GiThreeLeaves className='icon' />
+                            <p className={tog ? "title" : 'nunito text-[grey] text-[20px]'}>Repayment</p>
+                        </a>
+                        <a href='loan-history' className='flex gap-2 items-center'>
+                            <BiHistory className='icon' />
+                            <p className={tog ? "title" : 'nunito text-[grey] text-[20px]'}>Loan History</p>
+                        </a>
+                        <a href='payout' className='flex gap-2 items-center'>
+                            <FaAmazonPay className='icon' />
+                            <p className={tog ? "title" : 'nunito text-[grey] text-[20px]'} href="">Payout</p>
+                        </a>
+                        <a href='settings' className='flex gap-2 items-center'>
+                            <AiOutlineSetting className='icon' />
+                            <p className={tog ? "title" : 'nunito text-[grey] text-[20px]'} href="">Settings</p>
+                        </a>
+                    </div>
+                    <a href='logout' className='bg-[#35CD8D] flex gap-2 text-left pl-3 py-8'>
+                        <BiLogOutCircle className='icon' />
+                        <p className={tog ? "title" : 'nunito text-white text-[20px] font-extrabold'}>Log out</p>
+                    </a>
+                </div>
             </div>
-            <div className='flex flex-col h-[100%] justify-between'>
-                <div className='pl-3 lg:flex flex-col gap-12 px-3'>
+            {/* <div className={open ? "absolute w-[50%] h-[92%] justify-between flex flex-col bg-white ease-in duration-300" : 'lg:hidden bg-white w-[0] overflow-hidden md:w-0 flex flex-col h-[89%] justify-between ease-in duration-300 absolute'}>
+                <div className='pl-3 flex flex-col gap-12 mt-7'>
                     <a href='' className='flex gap-2 items-center '>
-                        <LuLayoutDashboard className='icon'/>
-                        <p className={tog? "title":'nunito text-[#35CD8D] text-[20px]'}>Dashboard</p>
+                        <LuLayoutDashboard className='icon' />
+                        <p className='nunito text-[#35CD8D] text-[20px]'>Dashboard</p>
                     </a>
                     <a href='' className='flex gap-2 items-center'>
-                        <GiThreeLeaves className='icon'/>
-                        <p className={tog? "title":'nunito text-[grey] text-[20px]'}>Farm Details</p>
+                        <GiThreeLeaves className='icon' />
+                        <p className='nunito text-[grey] text-[20px]'>Farm Details</p>
                     </a>
                     <a href='' className='flex gap-2 items-center'>
-                        <PiListNumbersBold className='icon'/>
-                        <p className={tog? "title":'nunito text-[grey] text-[20px]'}>Loan Application</p>
+                        <PiListNumbersBold className='icon' />
+                        <p className='nunito text-[grey] text-[20px]'>Loan Application</p>
                     </a>
                     <a href='' className='flex gap-2 items-center'>
-                        <GiThreeLeaves className='icon'/>
-                        <p className={tog? "title":'nunito text-[grey] text-[20px]'}>Repayment</p>
+                        <GiThreeLeaves className='icon' />
+                        <p className='nunito text-[grey] text-[20px]'>Repayment</p>
                     </a>
                     <a href='' className='flex gap-2 items-center'>
-                        <BiHistory className='icon'/>
-                        <p className={tog? "title":'nunito text-[grey] text-[20px]'}>Loan History</p>
+                        <BiHistory className='icon' />
+                        <p className='nunito text-[grey] text-[20px]'>Loan History</p>
                     </a>
                     <a href='' className='flex gap-2 items-center'>
-                        <FaAmazonPay className='icon'/>
-                        <p className={tog? "title":'nunito text-[grey] text-[20px]'} href="">Payout</p>
+                        <FaAmazonPay className='icon' />
+                        <p className='nunito text-[grey] text-[20px]' href="">Payout</p>
                     </a>
                     <a href='' className='flex gap-2 items-center'>
-                        <AiOutlineSetting className='icon'/>
-                        <p className={tog? "title":'nunito text-[grey] text-[20px]'} href="">Settings</p>
+                        <AiOutlineSetting className='icon' />
+                        <p className='nunito text-[grey] text-[20px]' href="">Settings</p>
                     </a>
                 </div>
                 <a href='' className='bg-[#35CD8D] flex gap-2 text-left pl-3 py-8'>
-                    <BiLogOutCircle className='icon'/>
-                    <p className={tog? "title":'nunito text-white text-[20px] font-extrabold'}>Log out</p>
+                    <BiLogOutCircle className='icon' />
+                    <p className={tog ? "title" : 'nunito text-white text-[20px] font-extrabold'}>Log out</p>
                 </a>
             </div>
-        </div>
-        <div className={open? "absolute w-[50%] h-[92%] justify-between flex flex-col bg-white ease-in duration-300":'lg:hidden bg-white w-[0] overflow-hidden md:w-0 flex flex-col h-[92%] justify-between ease-in duration-300 absolute'}>
-            <div className='pl-3 flex flex-col gap-12 mt-7'>
-                <a href='' className='flex gap-2 items-center '>
-                    <LuLayoutDashboard className='icon'/>
-                    <p className='nunito text-[#35CD8D] text-[20px]'>Dashboard</p>
-                </a>
-                <a href='' className='flex gap-2 items-center'>
-                    <GiThreeLeaves className='icon'/>
-                    <p className='nunito text-[grey] text-[20px]'>Farm Details</p>
-                </a>
-                <a href='' className='flex gap-2 items-center'>
-                    <PiListNumbersBold className='icon'/>
-                    <p className='nunito text-[grey] text-[20px]'>Loan Application</p>
-                </a>
-                <a href='' className='flex gap-2 items-center'>
-                    <GiThreeLeaves className='icon'/>
-                    <p className='nunito text-[grey] text-[20px]'>Repayment</p>
-                </a>
-                <a href='' className='flex gap-2 items-center'>
-                    <BiHistory className='icon'/>
-                    <p className='nunito text-[grey] text-[20px]'>Loan History</p>
-                </a>
-                <a href='' className='flex gap-2 items-center'>
-                    <FaAmazonPay className='icon'/>
-                    <p className='nunito text-[grey] text-[20px]' href="">Payout</p>
-                </a>
-                <a href='' className='flex gap-2 items-center'>
-                    <AiOutlineSetting className='icon'/>
-                    <p className='nunito text-[grey] text-[20px]'href="">Settings</p>
-                </a>
-            </div>
-            <a href='' className='bg-[#35CD8D] flex gap-2 text-left pl-3 py-8'>
-                <BiLogOutCircle className='icon'/>
-                <p className={tog? "title":'nunito text-white text-[20px] font-extrabold'}>Log out</p>
-            </a>
-        </div>
-        <div  className='lg:hidden absolute right-[13%] md:top-[10%] top-[14%]'>
-            <GiFarmTractor onClick={handleTracktor} className={open? 'translate-x-10 duration-700 ':"translate-x-0 duration-700"}/>
-        </div>
-    </>
-  )
+            {
+                hideTracktor && (
+                    <div className='lg:hidden absolute right-[13%] md:top-[10%] top-[14%]'>
+                        <GiFarmTractor onClick={handleTracktor} className={open ? 'translate-x-10 duration-700 ' : "translate-x-0 duration-700"} />
+                    </div>
+                )
+            } */}
+
+        </>
+    )
 }
 
 export default Leftnav
--- client-side/src/Components/Dashboard/Dashtop.jsx
+++ client-side/src/Components/Dashboard/Dashtop.jsx
@@ -1,23 +1,81 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AiOutlineSearch} from 'react-icons/ai'
+import {GiHamburgerMenu} from 'react-icons/gi'
 import alert from '../../Images/Group 18.png'
 import question from '../../Images/Group 19.png'
 import options from '../../Images/Frame 143.png'
+import logo from '../../Images/logo.png'
+import { LuLayoutDashboard } from 'react-icons/lu'
+// import toggle from '../../../Images/Group 20.png'
+import { GiThreeLeaves } from 'react-icons/gi'
+import { BiHistory, BiLogOutCircle } from 'react-icons/bi'
+import { FaAmazonPay } from 'react-icons/fa'
+import { AiOutlineSetting } from 'react-icons/ai'
+import { PiListNumbersBold } from 'react-icons/pi'
+import { GiFarmTractor } from 'react-icons/gi'
 
 const Dashtop = () => {
+  const [open, setOpen] = useState(false)
+  
+  const handleTracktor = () => {
+    setOpen(!open)
+}
   return (
+    <>
     <div className='w-full flex justify-between px-6 py-3 bg-white border-b-2'>
-        <h5>Logo</h5>
-        <div className='flex border-2 bg-[#b8b7b7] border-none w-[25%] gap-4 rounded-3xl px-3 py-[2px]'>
-            <AiOutlineSearch/>
-            <input className='outline-0 w-[80%] bg-transparent' type="text" placeholder='Search Here' />
+        <div>
+          <img className='hidden lg:block' src={logo} alt="" />
+          <GiHamburgerMenu onClick={handleTracktor} className='lg:hidden block'/>
         </div>
-        <div className='flex gap-3'>
-            <img className='w-[40px] h-[40px]' src={question} alt="" />
-            <img className='w-[40px] h-[40px]' src={alert} alt="" />
-            <img src={options} alt="" />
+        <div className='flex w-[70%]'>
+          <div className='flex border-2 bg-[#b8b7b7] border-none w-[25%] gap-4 rounded-3xl px-3 py-[2px]'>
+              <AiOutlineSearch/>
+              <input className='outline-0 w-[80%] bg-transparent' type="text" placeholder='Search Here' />
+          </div>
+          <div className='flex gap-3'>
+              <img className='w-[40px] h-[40px]' src={question} alt="" />
+              <img className='w-[40px] h-[40px]' src={alert} alt="" />
+              <img src={options} alt="" />
+          </div>
         </div>
     </div>
+    <div className={open ? "absolute w-[55%] h-[100%] overflow-hidden justify-between flex flex-col bg-white ease-in duration-300" : 'lg:hidden bg-white w-[0] overflow-hidden md:w-0 flex flex-col h-[89%] justify-between ease-in duration-300 absolute'}>
+    <div className='pl-3 flex flex-col gap-12 mt-7'>
+        <a href='' className='flex gap-2 items-center '>
+            <LuLayoutDashboard className='icon' />
+            <p className='nunito text-[#35CD8D] text-[20px]'>Dashboard</p>
+        </a>
+        <a href='' className='flex gap-2 items-center'>
+            <GiThreeLeaves className='icon' />
+            <p className='nunito text-[grey] text-[20px]'>Farm Details</p>
+        </a>
+        <a href='' className='flex gap-2 items-center'>
+            <PiListNumbersBold className='icon' />
+            <p className='nunito text-[grey] text-[20px]'>Loan Application</p>
+        </a>
+        <a href='' className='flex gap-2 items-center'>
+            <GiThreeLeaves className='icon' />
+            <p className='nunito text-[grey] text-[20px]'>Repayment</p>
+        </a>
+        <a href='' className='flex gap-2 items-center'>
+            <BiHistory className='icon' />
+            <p className='nunito text-[grey] text-[20px]'>Loan History</p>
+        </a>
+        <a href='' className='flex gap-2 items-center'>
+            <FaAmazonPay className='icon' />
+            <p className='nunito text-[grey] text-[20px]' href="">Payout</p>
+        </a>
+        <a href='' className='flex gap-2 items-center'>
+            <AiOutlineSetting className='icon' />
+            <p className='nunito text-[grey] text-[20px]' href="">Settings</p>
+        </a>
+    </div>
+    <a href='' className='bg-[#35CD8D] flex gap-2 text-left pl-3 py-8'>
+        <BiLogOutCircle className='icon' />
+        <p className='nunito text-white text-[20px] font-extrabold'>Log out</p>
+    </a>
+  </div>
+</>
   )
 }
 
