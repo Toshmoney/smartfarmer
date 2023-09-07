6e197c9fce2805b824b20eef8fa5a3a76bc999de
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-07 10:45:30 -0400

    updating setting page with tabs

--- client-side/src/Components/DashSettings/Securitypage.jsx
+++ client-side/src/Components/DashSettings/Securitypage.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Securitypage = () => {
   return (
-    <div className='bg-white w-full h-page px-7'>
+    <div className='bg-white w-full h-page px-7 nunito'>
         <div>
             <h1 className='nunito font-bold'>Security Management</h1>
             <p className='nunito '>Your Security Information show here</p>
             <div className='w-full'>
                 <div className='flex flex-row justify-between'>
                     <span>
-                        icon
+                        <div className='bg-[#35CD8D] items-center justify-center'>
+
+                        </div>
                         <div>
                             <h6>Password</h6>
                             <p>Your Security informaton Shows here</p>
