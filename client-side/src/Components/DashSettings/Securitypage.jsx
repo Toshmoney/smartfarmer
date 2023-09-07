136b1a39006c08f9dd46e76139efa3c33cf31028
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-07 11:04:23 -0400

    updating setting page with tabs

--- client-side/src/Components/DashSettings/Securitypage.jsx
+++ client-side/src/Components/DashSettings/Securitypage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import key from '../../Images/visibility.svg'
 
 const Securitypage = () => {
   return (
@@ -8,9 +9,9 @@ const Securitypage = () => {
             <p className='nunito '>Your Security Information show here</p>
             <div className='w-full'>
                 <div className='flex flex-row justify-between'>
-                    <span>
+                    <span className='flex gap-2'>
                         <div className='bg-[#35CD8D] items-center justify-center'>
-
+                            <img src={key} alt="" />
                         </div>
                         <div>
                             <h6>Password</h6>
