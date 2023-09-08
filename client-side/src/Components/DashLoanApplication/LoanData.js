31b4590ae1062be215f4512b728e6960e891d694
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-08 08:18:43 -0400

    updated loan application page

--- client-side/src/Components/DashLoanApplication/LoanData.js
+++ client-side/src/Components/DashLoanApplication/LoanData.js
@@ -24,6 +24,17 @@ export const columns = [
   {
     title: "Status",
     dataIndex: "LoanStatus",
+    render:(LoanStatus)=>{
+      if(LoanStatus=== "Approved"){
+      return(
+        <h6 className='text-[green] bg-[lightgreen]'>Approved</h6>
+      ) 
+      }else if(LoanStatus === "Pending"){
+        return(<h6 className='text-[yellow] bg-[lightyellow]'> Pending</h6>)
+      }else if( LoanStatus === "Failed"){
+        return (<h6 className='text-red-300 bg-[lightred]'>Failed</h6>)
+      }
+    },
     sorter:true
   },
   {
@@ -42,26 +53,98 @@ export const LoandData = [
     LoanID: 79214204,
     Amount: 'NGN 200,000.00',
     PhoneNumber: '+234913392374',
-    LoanStatus: 'Pending',
+    LoanStatus: 'Approved',
     LoanDate: '17 Oct, 2020',
-    action: <img src={del}/>
+    action: <img src={del} alt='' />
   },
   {
-    No:1,
+    No:2,
     LoanID: 79214204,
     Amount: 'NGN 200,000.00',
     PhoneNumber: '+224913392374',
     LoanStatus: 'Pending',
     LoanDate: '17 Oct, 2020',
-    action: <img src={del}/>
+    action: <img src={del} alt=''/>
   },
   {
-    No:1,
+    No:3,
     LoanID: 79214204,
     Amount: 'NGN 200,000.00',
     PhoneNumber: '+234813392374',
-    LoanStatus: 'Pending',
+    LoanStatus: 'Failed',
     LoanDate: '17 Oct, 2020',
-    action: <img src={del}/>
+    action: <img src={del} alt=''/>
+  },
+  {
+    No:4,
+    LoanID: 79214204,
+    Amount: 'NGN 200,000.00',
+    PhoneNumber: '+234813392374',
+    LoanStatus: 'Failed',
+    LoanDate: '17 Oct, 2020',
+    action: <img src={del} alt=''/>
+  },
+  {
+    No:5,
+    LoanID: 79214204,
+    Amount: 'NGN 200,000.00',
+    PhoneNumber: '+234813392374',
+    LoanStatus: 'Failed',
+    LoanDate: '17 Oct, 2020',
+    action: <img src={del} alt=''/>
+  },
+  {
+    No:6,
+    LoanID: 79214204,
+    Amount: 'NGN 200,000.00',
+    PhoneNumber: '+234813392374',
+    LoanStatus: 'Failed',
+    LoanDate: '17 Oct, 2020',
+    action: <img src={del} alt=''/>
+  },
+  {
+    No:7,
+    LoanID: 79214204,
+    Amount: 'NGN 200,000.00',
+    PhoneNumber: '+234813392374',
+    LoanStatus: 'Failed',
+    LoanDate: '17 Oct, 2020',
+    action: <img src={del} alt=''/>
+  },
+  {
+    No:8,
+    LoanID: 79214204,
+    Amount: 'NGN 200,000.00',
+    PhoneNumber: '+234813392374',
+    LoanStatus: 'Failed',
+    LoanDate: '17 Oct, 2020',
+    action: <img src={del} alt=''/>
+  },
+  {
+    No:9,
+    LoanID: 79214204,
+    Amount: 'NGN 200,000.00',
+    PhoneNumber: '+234813392374',
+    LoanStatus: 'Failed',
+    LoanDate: '17 Oct, 2020',
+    action: <img src={del} alt=''/>
+  },
+  {
+    No:10,
+    LoanID: 79214204,
+    Amount: 'NGN 200,000.00',
+    PhoneNumber: '+234813392374',
+    LoanStatus: 'Failed',
+    LoanDate: '17 Oct, 2020',
+    action: <img src={del} alt=''/>
+  },
+  {
+    No:11,
+    LoanID: 79214204,
+    Amount: 'NGN 200,000.00',
+    PhoneNumber: '+234813392374',
+    LoanStatus: 'Failed',
+    LoanDate: '17 Oct, 2020',
+    action: <img src={del} alt=''/>
   },
 ]
