295e4678be12af32e01f7b48c2b7998ec813e225
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-10 15:02:07 -0400

    updated landing page with animation

--- client-side/src/Components/DashSettings/Profilepage.jsx
+++ client-side/src/Components/DashSettings/Profilepage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import profile from '../../Images/setting prof.svg'
 import { TfiWorld } from 'react-icons/tfi'
-import { CiLocationOn } from 'react-icons/ci'
+import { CiLocationOn, CiMail } from 'react-icons/ci'
 import { BiUser } from 'react-icons/bi'
+import { BsSendSlash } from 'react-icons/bs'
+import {HiOutlineHome} from 'react-icons/hi'
+
 
 const Profilepage = () => {
     return (
@@ -41,23 +44,23 @@ const Profilepage = () => {
                     </div>
                     <div className='flex flex-col lg:gap-[5rem] gap-[1rem] w-full'>
                         <span>
-                            <h5>Full Name</h5>
+                            <h5>Email Address</h5>
                             <div className='border-2 border-[grey] flex px-1 gap-1 items-center rounded-2xl'>
-                                <BiUser className='w-[25px] h-[25px]' />
+                                <CiMail className='w-[25px] h-[25px]' />
                                 <input type="text" className='w-full  py-3 outline-none bg-transparent' placeholder='E.G John Doe' />
                             </div>
                         </span>
                         <span>
-                            <h5>Country</h5>
+                            <h5>State / Province</h5>
                             <div className='border-2 border-[grey] flex px-1 gap-1 items-center rounded-2xl'>
-                                <TfiWorld className='w-[25px] h-[25px]' />
+                                <BsSendSlash className='w-[25px] h-[25px]' />
                                 <input type="text" className='w-full py-3 outline-none bg-transparent' placeholder='Nigeria' />
                             </div>
                         </span>
                         <span>
-                            <h5>Full Name</h5>
+                            <h5>Street Address</h5>
                             <div className='border-2 border-[grey] flex px-1 gap-1 items-center rounded-2xl'>
-                                <CiLocationOn className='w-[25px] h-[25px]' />
+                                <HiOutlineHome className='w-[25px] h-[25px]' />
                                 <input type="text" className='w-full py-3 outline-none bg-transparent' placeholder='Ilorin' />
                             </div>
                         </span>
