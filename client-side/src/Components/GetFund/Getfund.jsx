63276bffc0b585d706e875140fe0efc93d1ea6c7
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-10 09:33:51 -0400

    Adding animation

--- client-side/src/Components/GetFund/Getfund.jsx
+++ client-side/src/Components/GetFund/Getfund.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Getfundprops from './Getfundprops'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Getfund = () => {
+    useEffect(()=>{
+        AOS.init();
+    },[])
   return (
     <div className='container px-[20px] lg:px-0 p-x-xl flex flex-col lg:flex lg:flex-row lg:gap-x-[12rem]'>
-        <div className='w-full md:w-full text-center flex flex-col gap-3 bg-green-200 rounded-3xl lg:w-1/2 md:text-center lg:text-left justify-center px-10 mb-11 py-10'>
+        <div data-aos="flip-up" className='w-full md:w-full text-center flex flex-col gap-3 bg-green-200 rounded-3xl lg:w-1/2 md:text-center lg:text-left justify-center px-10 mb-11 py-10'>
             <h3 className='nunito text-[20px] md:text-[30px] lg:w-[70%]'>The Best place to get Fund for your Agriculturer Business</h3>
             <p className='word text-[15px] lg:w-[78%]'>We Give Loans to Founders, Business Owners, Farmers to Grow their Business in a Profitable way</p>
             <div className='w-full md:w-full text-center lg:text-left'>
                 <button className='btn text-white'>Get Started</button>   
             </div>
         </div>
-        <div className=' lg:grid grid-cols-2 gap-6 lg:w-1/2 flex flex-col md:justify-center md:text-center'>
+        <div data-aos="fade-left" className=' lg:grid grid-cols-2 gap-6 lg:w-1/2 flex flex-col md:justify-center md:text-center'>
             <Getfundprops
                 number = '01.'
                 title = "Ease And Efficiency"
--- client-side/src/Components/Hero/Hero.jsx
+++ client-side/src/Components/Hero/Hero.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import hero from '../../Images/HeroImg (1).png'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 const Hero = () =>{
+    useEffect(()=>{
+        AOS.init();
+
+    },[])
     return(
         <div className="lg:px-0 px-4">
             <div className=' container  flex flex-col md:flex-col w-full lg:flex lg:w-full lg:flex-row items-center relative'>
-                <div className='w-full flex-col items-center lg:w-1/2 lg:text-left text-center space-y-5'>
+                <div data-aos="fade-right" className='w-full flex-col items-center lg:w-1/2 lg:text-left text-center space-y-5'>
                     <h1 className='nunito w-[100%] text-[25px] md:text-[30px] lg:w-[55%]  md:w-[100%] lg:text-4xl '>Supporting Africa Farmer to Fund their Agric Business</h1>
                     <p className="nunito text-[gray]">Easy Access to 5million worth of Loan for Your Business</p>
                     <div>
                         <button className='btn text-white'>Get Started</button>
                     </div>
                 </div>
-                <img className='w-1/2 md:[75%] lg:w-1/2' src={hero} alt="" />
+                <img data-aos="fade-left" className='w-1/2 md:[75%] lg:w-1/2' src={hero} alt="" />
                 <div className='absolute w-[50%] h-[70vh] bg-[#9fddc3] right-20 blur-3xl -z-20 rounded-full opacity-30'></div>
             </div>
-            <div className="grid grid-cols-2 text-center bg-[#EBFBF3] py-12 mt-[2rem] md:mt-[2rem] lg:mt-[4rem] mb-[3rem] lg:grid-cols-4 gap-4">
+            <div data-aos="slide-up" className="grid grid-cols-2 text-center bg-[#EBFBF3] py-12 mt-[2rem] md:mt-[2rem] lg:mt-[4rem] mb-[3rem] lg:grid-cols-4 gap-4">
                 <div>
                     <p className="nunito lg:text-[20px] font-bold text-[grey] text-[15px]">Access to Over</p>
                     <h1 className="nunito lg:text-[40px] text-[25px]">N50N+</h1>
